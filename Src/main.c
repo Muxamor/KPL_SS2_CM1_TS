7c41b7b2f3bc94c4dd2982e3fe6cd8e47075ec1d
Muxamor <ivan.neskorodev@gmail.com>
2018-10-14 22:03:57 +0700

    Creat

---
+++ Src/main.c
@@ -0,0 +1,25 @@
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "SetupPeriph.h"
+
+#include  <stdio.h>
+
+/****************************TODO*************************
+
+**********************************************************/
+//LL_mDelay(1);
+//LL_RCC_ClocksTypeDef check_RCC_Clocks,  *CHECK_RCC_CLOCKS=&check_RCC_Clocks; // Only for check setup clock. Not need use in release
+
+
+int main(void){
+
+	LL_Init();
+	SystemClock_Config(); //Setup system clock at 80 MHz
+
+
+
+
+}
+
+
