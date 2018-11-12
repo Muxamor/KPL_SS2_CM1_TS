82358ebce76182b61aaa459273b0dfd2660d01cf
Ivan Neskorodev <ivan.neskorodev@gmail.com>
2018-11-12 23:27:04 +0700

    change tine daley USART ADC

--- Src/uart_comm.c
+++ Src/uart_comm.c
@@ -107,7 +107,7 @@ ErrorStatus ADC_data_receive_UART (uint8_t receive_data[], uint8_t size_rec_data
 
 			counter++;
 
-			if(counter==100000){
+			if(counter==200000){
 				
 				for(i=0; i<size_rec_data; i++){
 					if(i==0){
